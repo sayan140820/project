94c7e4aecb9bf623115d2e99efa70f381a3ccc0a
sayan140820 <90931350+sayan140820@users.noreply.github.com>
2023-01-03 19:22:32 +0530

    Add files via upload

---
+++ project01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number, guess, nguesses = 1;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    
+    do
+    {
+        printf("guess the number between 1 to 100\n");
+        scanf("%d", &guess);
+
+        if (guess > number)
+        {
+            printf("lower number please\n");
+        }
+        else if(guess < number)
+        {
+            printf("higher number please\n");
+        }
+        else
+        {
+            printf("you guessed the number in %d attemts\n", nguesses);
+        }
+        nguesses++;
+    } while (guess != number);
+
+    return 0;
+}
